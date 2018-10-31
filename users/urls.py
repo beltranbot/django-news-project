289bbfcac15436465426536f3d4494d670678393
Eduardo Eeltran <beltranbot@gmail.com>
2018-10-31 09:16:22 -0500

    added tests

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signupo/', views.SignUp.as_view(), name='signup'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]
